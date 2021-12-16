d318bbd5609a4aa79300f7a0193f0955d9297f4a
Shahzad <shasa3f@ad.vzwcorp.com>
2021-12-16 14:12:25 -0500

    updated methods

--- models/index.js
+++ models/index.js
@@ -1,11 +1,15 @@
-const {Sequelize, DataTypes, Model} = require('sequelize');
+const {Sequelize, DataTypes, Model, INTEGER} = require('sequelize');
 const {sequelize} = require('../db');
 
 class Sauce extends Model {}
 
 Sauce.init({
     name: DataTypes.STRING,
-    image: DataTypes.STRING
+    image: DataTypes.STRING,
+    likes:{
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
 }, {
     sequelize,
     timestamps: false
--- server.js
+++ server.js
@@ -8,13 +8,11 @@ const Handlebars = require('handlebars');
 const expressHandlebars = require('express-handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access') 
 
-
 //Import our database and model
 const {sequelize} = require('./db');
 const {Sauce} = require('./models/index');
 
-
-const seed = require('./seed')
+const seed = require('./seed');
 
 //Set up our templating engine with handlebars
 const handlebars = expressHandlebars({
@@ -26,25 +24,68 @@ app.set('view engine', 'handlebars'); // To render template files, set the follo
 //serve static assets from public folder
 app.use(express.static('public')) //
 
-//body parser so req.body is not undefined
-app.use(require('body-parser').urlencoded());
+//allow express to read json request bodies
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 //seed our database
 seed();
 
 //*************** ROUTES ******************//
+//index redirects to sauces
+app.get('/', (req,res)=>{
+    res.redirect('/sauces')
+})
+
+//get all sauces
 app.get('/sauces', async (req, res) => {
     const sauces = await Sauce.findAll();
     res.render('sauces', {sauces}); //first param points to the sauces view in handlebars, second param is the data from the db
 })
 
+//get sauce by id
 app.get('/sauces/:id', async (req, res) => {
     const sauce = await Sauce.findByPk(req.params.id);
     res.render('sauce', {sauce}); //sauce hb view
 })
 
-//New Routes go here: 
+//update sauce by id
+app.put('/sauces/:id', async (req,res) => {
+    let updatedSauce = await Sauce.update(req.body, {
+        where: {id: req.params.id}
+    })
+    const sauce = await Sauce.findByPk(req.params.id)
+    res.render('sauce', {sauce})
+})
 
+//New Routes go here: 
+app.get('/new-sauce', async (req, res) => {
+    res.render('newSauceForm')
+})
+
+//Post Route triggered by form submit action
+app.post('/new-sauce', async (req,res) =>{
+    //Add sauce to db based on html form data
+    const newSauce = await Sauce.create(req.body)
+    //Create a sauceAlert to pass to the template
+    let sauceAlert = `${newSauce.name} added to your database`
+    //Find newSauce in db by id
+    const foundSauce = await Sauce.findByPk(newSauce.id)
+    if(foundSauce){
+        res.render('newSauceForm',{sauceAlert})
+    } else {
+        sauceAlert = 'Failed to add Sauce'
+        res.render('newSauceForm',{sauceAlert})
+    }
+})
+
+//DELETE method, sauces/:id path => Deletes a sauce from db.sqlite
+app.delete('/sauces/:id', async (req,res)=>{
+    const deletedSauce = await Sauce.destroy({
+        where: {id:req.params.id}
+    })
+    res.send(deletedSauce ? 'Deleted' : 'Deletion Failed')
+})
 
 //serving is now listening to PORT
 app.listen(PORT, () => {
